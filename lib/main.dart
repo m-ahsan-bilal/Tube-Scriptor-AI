bf8fa9893426485330f369204729f3ac48735dfb
ahsan <ahsan123.bilal@gmail.com>
2025-01-14 23:58:36 +0500

    app_set

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tube_scriptor_ai/utils/go_router.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: appRoutesList,
+    
+    );
   }
 }
---
+++ lib/utils/go_router.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tube_scriptor_ai/landing/splash_screen.dart';
+import 'package:tube_scriptor_ai/screens/home.dart';
+import 'package:tube_scriptor_ai/screens/scriptor.dart';
+import 'package:tube_scriptor_ai/screens/settings_page.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+final GoRouter basicRoutes = GoRouter(
+  routes: appRoutesList,
+  initialLocation: "/",
+  debugLogDiagnostics: true,
+  navigatorKey: navigatorKey,
+  errorBuilder: (context, state) => Center(
+    child: Container(
+      color: Colors.white,
+      child: Text(state.error.toString()),
+    ),
+  ),
+);
+List<RouteBase> appRoutesList = [
+  GoRoute(
+    path: "/",
+    name: "splash",
+    builder: (context, state) => SplashScreen(),
+  ),
+  GoRoute(
+    path: "/home",
+    name: "home page",
+    builder: (context, state) => const HomePage(),
+  ),
+  GoRoute(
+    path: "/scriptor",
+    name: "scriptor page",
+    builder: (context, state) => const ScriptorPage(),
+  ),
+  GoRoute(
+    path: "/settings",
+    name: "settings page",
+    builder: (context, state) => const SettingsPage(),
+  ),
+];
+
+class LandingPage {
+  const LandingPage();
+}
